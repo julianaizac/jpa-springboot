f86acf8452e603570083fabeaf8d2be19df329b5
julianaizac <dev.julianaizac@outlook.com>
2021-03-09 11:19:05 -0300

    Initial commit

---
+++ src/main/java/com/julianaizac/jpaspringboot/JpaSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.julianaizac.jpaspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaSpringbootApplication.class, args);
+	}
+
+}
